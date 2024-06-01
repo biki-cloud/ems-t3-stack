5cbba74a1d50a6b14b308a4ee14aa147d3e207b8
hibiki <hibiki123123@gmail.com>
2024-06-01 16:29:20 +0900

    add getBaseUrl():

--- trpc/url.tsx
+++ trpc/url.tsx
@@ -1,7 +1,7 @@
 export function getBaseUrl() {
     //process.env.VERCEL_URLがセットされている場合は、そのURLを返す
-    if (process.env.VERCEL_BRANCH_URL) return `https://${process.env.VERCEL_BRANCH_URL}`;
-    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+    if (process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL) return `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`;
+    if (process.env.NEXT_PUBLIC_VERCEL_URL) return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
     //それ以外の場合は、ローカルのURLを返す
     return `${process.env.NEXT_PUBLIC_APP_URL}`;
   }
