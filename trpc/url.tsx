7d5f121e2b5aa38d7f9495aa99c6c5355014c3d2
hibiki <hibiki123123@gmail.com>
2024-06-01 19:51:57 +0900

    console2

--- trpc/url.tsx
+++ trpc/url.tsx
@@ -1,12 +1,21 @@
 export function getBaseUrl() {
+  console.log('process.env.VERCEL_ENV', process.env.VERCEL_ENV)
   console.log('process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL', process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL)
   console.log('process.env.NEXT_PUBLIC_VERCEL_URL', process.env.NEXT_PUBLIC_VERCEL_URL)
   console.log('process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL', process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL)
   console.log('process.env.VERCEL_URL', process.env.VERCEL_URL)
-  //process.env.VERCEL_URLがセットされている場合は、そのURLを返す
-  if (process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL) return `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`;
-  if (process.env.NEXT_PUBLIC_VERCEL_URL) return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
-  if (process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL) return `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`;
-  //それ以外の場合は、ローカルのURLを返す
-  return `${process.env.NEXT_PUBLIC_APP_URL}`;
+  if (process.env.VERCEL_ENV === 'development') {
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`;
+  } else if (process.env.VERCEL_ENV === 'production') {
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`;
+  } else {
+    // ローカルの場合は、ローカルのURLを返す
+    return `${process.env.NEXT_PUBLIC_APP_URL}`;
+  }
+  // //process.env.VERCEL_URLがセットされている場合は、そのURLを返す
+  // if (process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL) return `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`;
+  // if (process.env.NEXT_PUBLIC_VERCEL_URL) return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  // if (process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL) return `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`;
+  // //それ以外の場合は、ローカルのURLを返す
+  // return `${process.env.NEXT_PUBLIC_APP_URL}`;
 }
