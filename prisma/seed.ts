472e3eab7cebd5f64ed5381afe0d0bbe849d6192
hibiki <hibiki123123@gmail.com>
2024-06-07 21:38:41 +0900

    add seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,40 +1,127 @@
 import bcrypt from "bcrypt";
-import { PrismaClient } from "@prisma/client";
+import { Event, PrismaClient, User } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function main() {
-  const user1 = await prisma.user.create({
+async function createUser(
+  email: string,
+  name: string,
+  role: string,
+  image: string,
+  isAdmin = false,
+) {
+  return await prisma.user.create({
     data: {
-      email: "organizer1@example.com",
+      email,
       hashedPassword: await bcrypt.hash("password", 12),
-      name: "イベント主催者1",
-      role: "organizer",
-      isAdmin: true,
+      name,
+      role,
+      isAdmin,
+      image,
     },
   });
+}
 
-  await prisma.organizer.create({
+async function createOrganizer(user: User) {
+  return await prisma.organizer.create({
     data: {
-      userId: user1.id,
-      organizationName: "organizer-" + user1.id,
+      userId: user.id,
+      organizationName: "organizer-" + user.id,
     },
   });
+}
 
-  const event1 = await prisma.event.create({
+async function createVendor(user: User) {
+  return await prisma.vendor.create({
     data: {
-      userId: user1.id,
-      title: "スイーツパラダイス",
-      content: "スイーツが大好きな方、集まれ！",
-      location: "東京都渋谷区",
-      image:
-        "https://res.cloudinary.com/du8k76ffm/image/upload/v1717750265/t3stackblog/wqvmtfc4cq2di7cbwz8c.jpg",
+      userId: user.id,
+      vendorName: "vendor-" + user.id,
     },
   });
+}
 
-  const event2 = await prisma.event.create({
+async function createCustomer(user: User) {
+  return await prisma.customer.create({
     data: {
-      userId: user1.id,
+      userId: user.id,
+      customerName: "customer-" + user.id,
+    },
+  });
+}
+
+async function createEvent(
+  user: User,
+  title: string,
+  content: string,
+  location: string,
+  image: string,
+  premium = false
+) {
+  return await prisma.event.create({
+    data: {
+      userId: user.id,
+      title,
+      content,
+      location,
+      image,
+      premium,
+    },
+  });
+}
+
+async function createComment(user: User, event: Event, content: string) {
+  return await prisma.comment.create({
+    data: {
+      userId: user.id,
+      eventId: event.id,
+      content,
+    },
+  });
+}
+
+async function main() {
+  const organizer1 = await createUser(
+    "organizer1@example.com",
+    "イベント主催者1",
+    "organizer",
+    "https://res.cloudinary.com/du8k76ffm/image/upload/v1717763104/t3stackblog/zztfp01ft3okcun69hyk.png",
+    true
+  );
+  await createOrganizer(organizer1);
+  const event1 = await createEvent(
+    organizer1,
+    "スイーツパラダイス",
+    "スイーツが大好きな方、集まれ！",
+    "東京都渋谷区",
+    "https://res.cloudinary.com/du8k76ffm/image/upload/v1717750265/t3stackblog/wqvmtfc4cq2di7cbwz8c.jpg"
+  );
+  const event4 = await createEvent(
+    organizer1,
+    "天下一舞踏会やります。",
+    "サイヤ人に関わらず参加可能",
+    "ナメック星",
+    "https://res.cloudinary.com/du8k76ffm/image/upload/v1717763665/t3stackblog/tdhls4hoppcf85wifgxr.jpg",
+  );
+
+
+  const organizer2 = await createUser(
+    "organizer2@example.com",
+    "イベント主催者2",
+    "organizer",
+    "https://res.cloudinary.com/du8k76ffm/image/upload/v1717763204/t3stackblog/bh7c3mnntuvnbccizepx.png",
+    true
+  );
+  await createOrganizer(organizer2);
+  const event2 = await createEvent(
+    organizer2,
+    "フラワー鑑賞",
+    "フラワーが大好きな方、集まれ！",
+    "千葉県",
+    "https://res.cloudinary.com/du8k76ffm/image/upload/v1717751149/t3stackblog/bjwk0ypuhpii2mc5bq6p.jpg"
+  );
+  const event3 = await prisma.event.create({
+    data: {
+      userId: organizer2.id,
       title: "潮干狩り",
       content: "潮干狩りを楽しもう！",
       location: "千葉県木更津市",
@@ -44,98 +131,29 @@ async function main() {
     },
   });
 
-  const comment = await prisma.comment.create({
-    data: {
-      userId: user1.id,
-      eventId: event1.id,
-      content: "スイーツが大好きです！",
-    },
-  });
+  const vendor1 = await createUser(
+    "vendor@example.com",
+    "イベント出店者1",
+    "vendor",
+    "https://res.cloudinary.com/du8k76ffm/image/upload/v1717763250/t3stackblog/v10o9hjrdhlip0itkjdy.jpg"
+  );
+  await createVendor(vendor1);
 
-  const user2 = await prisma.user.create({
-    data: {
-      email: "organizer2@example.com",
-      hashedPassword: await bcrypt.hash("password", 12),
-      name: "イベント主催者2",
-      role: "organizer",
-      isAdmin: true,
-    },
-  });
-
-  await prisma.organizer.create({
-    data: {
-      userId: user2.id,
-      organizationName: "organizer-" + user1.id,
-    },
-  });
-
-  const event3 = await prisma.event.create({
-    data: {
-      userId: user2.id,
-      title: "フラワー鑑賞",
-      content: "フラワーが大好きな方、集まれ！",
-      location: "千葉県",
-      image:
-        "https://res.cloudinary.com/du8k76ffm/image/upload/v1717751149/t3stackblog/bjwk0ypuhpii2mc5bq6p.jpg",
-      premium: false,
-    },
-  });
-
-  const user4 = await prisma.user.create({
-    data: {
-      email: "vendor@example.com",
-      hashedPassword: await bcrypt.hash("password", 12),
-      name: "イベント出店者1",
-      role: "vendor",
-      isAdmin: true,
-    },
-  });
-
-  await prisma.vendor.create({
-    data: {
-      userId: user4.id,
-      vendorName: "vendor-" + user1.id,
-    },
-  });
-
-  const comment2 = await prisma.comment.create({
-    data: {
-      userId: user4.id,
-      eventId: event2.id,
-      content: "潮干狩りよき！",
-    },
-  });
-
-  const user6 = await prisma.user.create({
-    data: {
-      email: "customer1@example.com",
-      hashedPassword: await bcrypt.hash("password", 12),
-      name: "イベント参加者1",
-      role: "customer",
-    },
-  });
-
-  const customer1 = await prisma.customer.create({
-    data: {
-      userId: user6.id,
-      CustomerName: "customer-" + user6.id
-    },
-  });
-  await prisma.comment.create({
-    data: {
-      userId: user6.id,
-      eventId: event1.id,
-      content: "スイーツ食べたい！",
-    },
-  });
+  const customer1 = await createUser(
+    "customer1@example.com",
+    "イベント参加者1",
+    "customer",
+    "https://res.cloudinary.com/du8k76ffm/image/upload/v1717763285/t3stackblog/mk1kdovvoc7zbcocideo.jpg"
+  );
+  await createCustomer(customer1);
+  createComment(customer1, event1, "楽しみです！");
+  createComment(vendor1, event1, "行きたいです！");
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
+  .catch((e) => {
+    throw e;
   })
-  .catch(async (e) => {
-    console.error(e);
+  .finally(async () => {
     await prisma.$disconnect();
-    process.exit(1);
   });
