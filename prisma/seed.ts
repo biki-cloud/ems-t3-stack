698722d67bd5d59a2fa8064b645375ef3a0555ca
hibiki <hibiki123123@gmail.com>
2024-06-07 17:59:40 +0900

    add seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,26 +1,52 @@
-import prisma from "@/lib/prisma"
 import bcrypt from "bcrypt"
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 async function main() {
-  console.log('Seeding data...');
-
   const user1 = await prisma.user.create({
     data: {
       email: 'organizer1@example.com',
       hashedPassword: await bcrypt.hash('password', 12),
       name: 'イベント主催者1',
       role: 'organizer',
+      isAdmin: true
     },
   });
-  console.log('Created user:', user1);
-  
-  console.log('Data seeding complete.');
+
+  await prisma.organizer.create({
+    data: {
+      userId: user1.id,
+      organizationName: "organizer-" + user1.id
+    },
+  })
+
+  const event = await prisma.event.create({
+    data: {
+      userId: user1.id,
+      title: 'スイーツパラダイス',
+      content: 'スイーツが大好きな方、集まれ！',
+      location: '東京都渋谷区',
+      image: 'https://res.cloudinary.com/du8k76ffm/image/upload/v1717750265/t3stackblog/wqvmtfc4cq2di7cbwz8c.jpg'
+    }
+  })
+
+  const comment = await prisma.comment.create({
+    data: {
+      userId: user1.id,
+      eventId: event.id,
+      content: 'スイーツが大好きです！'
+    },
+  })
 }
 
+
 main()
-  .catch(err => {
-    console.error('Error seeding data:', err);
-  })
-  .finally(async () => {
+  .then(async () => {
     await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
   });
