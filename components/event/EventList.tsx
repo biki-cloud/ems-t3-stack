d4009b7ce49c480c9c39600cb29ba23f67071cba
hibiki <hibiki123123@gmail.com>
2024-06-14 17:58:44 +0900

    create participation request

--- components/event/EventList.tsx
+++ components/event/EventList.tsx
@@ -1,4 +1,6 @@
-import { trpc } from "@/trpc/client"
+'use client'
+// import { trpc } from "@/trpc/client"
+import { trpc } from "@/trpc/react"
 import PaginationButton from "../pagers/PaginationButton"
 import { Button } from "../ui/button"
 import { Input } from "../ui/input"
@@ -10,20 +12,26 @@ interface props {
     offset: number
 }
 
-const EventList = async ({ limit, offset }: props) => {
-    const { events: events, totalEvents: totalEvents } = await trpc.event.getEvents({
+const EventList = ({ limit, offset }: props) => {
+    const { data: events, isLoading } = trpc.event.getEvents.useQuery({
         limit,
         offset,
     })
 
     // 投稿がない場合
-    if (events.length === 0) {
+    if (events?.events.length === 0) {
         return (
             <div className="text-center text-sm text-gray-500">投稿はありません</div>
         )
     }
 
+    const totalEvents = events?.totalEvents
+    if (!totalEvents) return null
+
     const pageCount = Math.ceil(totalEvents / limit)
+
+    if (isLoading) return <div>Loading...</div>;
+    if (!events) return <div>No events found.</div>;
 
     return (
         <div className="space-y-5">
@@ -32,12 +40,19 @@ const EventList = async ({ limit, offset }: props) => {
                 <Button variant="default" >検索</Button>
             </div>
             <div className="space-y-5">
-                {events.map((event) => (
-                    <EventItem key={event.id} event={event} />
+                {events.events.map((event) => (
+                    <EventItem
+                        key={event.id}
+                        event={{
+                            ...event,
+                            createdAt: new Date(event.createdAt),
+                            updatedAt: new Date(event.updatedAt)
+                        }}
+                    />
                 ))}
             </div>
 
-            {events.length !== 0 && (
+            {events.events.length !== 0 && (
                 <PaginationButton pageCount={pageCount} displayPerPage={eventPerPage} />
             )}
         </div>
