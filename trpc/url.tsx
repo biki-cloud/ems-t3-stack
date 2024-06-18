350b364f80173cb4e21262536cdb7bc60e8d49f2
hibiki <hibiki123123@gmail.com>
2024-06-18 22:30:18 +0900

    create participation request

--- trpc/url.tsx
+++ trpc/url.tsx
@@ -7,7 +7,9 @@ export function getBaseUrl() {
   if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
     url = `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`;
   } else if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') {
-    url = `https://www.sunnybe.online`;
+    // 独自ドメイン
+    url = 'https://www.sunnybe.online';
+    // Vercelのデフォルトドメイン
     // url = `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`;
   } else {
     // ローカルの場合は、ローカルのURLを返す
