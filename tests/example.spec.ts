2d1fd85a7111270f94432ead32dad0ab3165a734
hibiki <hibiki123123@gmail.com>
2024-06-24 18:38:34 +0900

    cr

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,12 +2,27 @@ import { test, expect } from '@playwright/test';
 
 const BASE_URL = process.env.BASE_URL;
 
-test('verify title of the page', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   if (!BASE_URL) {
     throw new Error('URL is not provided');
   }
   await page.goto(BASE_URL);
+});
 
-  await expect(page).toHaveTitle('イベントマッチングサービス');
+test.describe('Search Event', () => {
+  test('search event by word', async ({ page }) => {
+    await expect(page).toHaveTitle('イベントマッチングサービス');
+    // const search = page.getByPlaceholder('イベント名');
+    // await search.fill("セル");
 
+    // const searchButton = page.getByRole('button', { name: '検索' });
+    // await searchButton.click();
+
+    // // イベントのリストを取得
+    // const eventList = await page.locator('div:has-text("イベント一覧")').locator('li');
+
+    // // イベントの数をアサート
+    // const eventCount = await eventList.count();
+    // expect(eventCount).toBe(4);  // 期待されるイベントの数を設定
+  })
 });
