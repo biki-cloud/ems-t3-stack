acf2d6fc6272d43ef0d67a518e59276493f1fdbb
hibiki <hibiki123123@gmail.com>
2024-06-28 22:26:24 +0900

    cr

--- components/settings/FootNavBar.tsx
+++ components/settings/FootNavBar.tsx
@@ -12,27 +12,8 @@ interface Props {
 }
 
 const FootNavBar = ({ items }: Props) => {
-  const [isVisible, setIsVisible] = useState(true);
-  let lastScrollY = 0;
-
-  const handleScroll = () => {
-    if (window.scrollY < lastScrollY) {
-      setIsVisible(true);
-    } else {
-      setIsVisible(false);
-    }
-    lastScrollY = window.scrollY;
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
   return (
-    <div className={`fixed bottom-0 w-full flex justify-around bg-white border-t border-gray-200 p-4 transition-transform duration-300 ${isVisible ? 'translate-y-0' : 'translate-y-full'}`}>
+    <div className={`fixed bottom-0 w-full flex justify-around bg-white border-t border-gray-200 p-4 transition-transform duration-300`}>
       {items.map((item) => (
         <Link href={item.href} key={item.title} className="text-center text-black hover:text-gray-500 text-sm">
           {item.title}
